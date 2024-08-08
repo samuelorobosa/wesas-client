20cd58896d20f01799f68b252a247b29789b3593
“Samuel <amagbakhensamuel@gmail.com>
2024-08-08 14:13:13 +0100

    Done: Finalizing codebase

--- src/core/utils/getBaseUrlFromEnv.js
+++ src/core/utils/getBaseUrlFromEnv.js
@@ -2,7 +2,6 @@ export const getBaseUrlFromEnv = () => {
   const urlFromEnvironment = {
     development: import.meta.env.VITE_PUBLIC_URL,
     production: import.meta.env.VITE_PUBLIC_URL,
-    testing: import.meta.env.VITE_PUBLIC_URL,
   };
   const env = import.meta.env.MODE;
   return urlFromEnvironment[env];
